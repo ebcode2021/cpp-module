2090a2bcba0d82c58c4b28e16c27952742d3b984
ebcode2021 <ebcode2021@gmail.com>
2023-01-25 21:35:14 +0900

    solve::ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,28 @@
+//class Name : 대문자 + 카멜케이스
+//내부 변수들은 : 소문자 + 카멜케이스
+
+// toupper example (C++)
+#include <iostream>       // std::cout
+#include <string>         // std::string
+
+void	megaphone(char *argv[])
+{
+	std::locale loc;
+	while (*argv)
+	{
+		std::string str(*argv);
+		for (int i = 0; i < str.length(); i++)
+			std::cout << std::toupper(str[i], loc);
+		argv++;
+	}
+	std::cout << std::endl;
+}
+
+int	main (int argc, char *argv[])
+{
+	if (argc > 1)
+		megaphone(++argv);
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  return 0;
+}
