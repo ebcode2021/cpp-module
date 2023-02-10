80c81c0a25fad7346d892a377f50d5a8f52431cd
ebcode2021 <ebcode2021@gmail.com>
2023-02-10 18:54:47 +0900

    modify::debug

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: eunson <eunson@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 20:19:50 by eunbison          #+#    #+#             */
-/*   Updated: 2023/02/05 20:28:41 by eunson           ###   ########.fr       */
+/*   Updated: 2023/02/10 18:54:24 by eunson           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int main(void)
 
 	std::cout << "[DEBUG]" << std::endl;
 	harl.complain("DEBUG");
-	std::cout << "---------------" << std::endl;
+	std::cout << "---------------" << std::endl; 
 	
 	std::cout << "[INFO]" << std::endl;
 	harl.complain("INFO");
