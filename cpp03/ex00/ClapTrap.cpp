1a938b7d62947b0ef526d8ada96fc1d88770af43
ebcode2021 <ebcode2021@gmail.com>
2023-02-16 22:41:00 +0900

    solve::cpp03-ex00

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -5,7 +5,7 @@
 ClapTrap::ClapTrap()
 {
 	std::cout << "call :: Default Constructor" << std::endl;
-	this->_name = "Eunson";
+	this->_name = "default";
 	this->_hit_points = 10;
 	this->_energy_points = 10;
 	this->_attack_damage = 0;
@@ -49,4 +49,74 @@ ClapTrap::~ClapTrap()
 std::string	ClapTrap::getName() const
 {
 	return (this->_name);
+}
+
+unsigned int ClapTrap::getHitPoints() const
+{
+	return (this->_hit_points);
+}
+
+unsigned int ClapTrap::getEnergyPoints() const
+{
+	return (this->_energy_points);
+}
+
+unsigned int ClapTrap::getAttackDamage() const
+{
+	return (this->_attack_damage);
+}
+
+// method
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (this->getHitPoints() <= 0 || this->getEnergyPoints() <= 0)
+	{
+		std::cout << "Not Enough HP || Not Enough EP" << std::endl;
+		return ;
+	}
+	std::cout << "ClapTrap " << this->getName() << " attacks " << target \
+				<< ", causing " << this->getAttackDamage() << " points of damage!" << std::endl;
+	this->_energy_points -= 1;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (this->getHitPoints() <= 0)
+	{
+		std::cout << "Already dead.." << std::endl;
+		return ;
+	}
+	std::cout << "ClapTrap " << this->getName() << " take " << amount \
+				<< " points of damage!" << std::endl;
+	if (this->_hit_points < amount)
+		this->_hit_points = 0;
+	else
+		this->_hit_points -= amount;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (this->getHitPoints() <= 0)
+	{
+		std::cout << "Already dead.." << std::endl;
+		return ;
+	}
+	if (this->getEnergyPoints() <= 0)
+	{
+		std::cout << "Nope!" << std::endl;
+		return ;
+	}
+	std::cout << "ClapTrap " << this->getName() << " take " << amount \
+				<< " points of repaired!" << std::endl;
+	this->_energy_points -= 1;
+	this->_hit_points += amount;
+}
+
+void	ClapTrap::printStatus() const
+{
+	std::cout << "[ " << this->_name<< " ]" << std::endl;
+	std::cout << " - Hit points : " << this->_hit_points << std::endl;
+	std::cout << " - Energy points : " << this->_energy_points << std::endl;
+	std::cout << " - Attack damage : " << this->_attack_damage << std::endl;
 }
--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -3,6 +3,49 @@
 
 int	main()
 {
-	ClapTrap	clap1;
+	ClapTrap	default_clap;
+	ClapTrap	eunson_clap("eunson");
+	ClapTrap	hp_clap("hp_clap");
+	ClapTrap	energy_clap("energy_clap");
+
+	// 초기상태
+	std::cout << "------------- 초기상태 -------------" << std::endl;
+	default_clap.printStatus();
+	eunson_clap.printStatus();
+	hp_clap.printStatus();
+	energy_clap.printStatus();
+
+	// 수리중
+	std::cout <<  "------------- 수리중 -------------" << std::endl;
+	default_clap.beRepaired(5);
+	eunson_clap.beRepaired(7);
+	hp_clap.beRepaired(9);
+	energy_clap.beRepaired(10);
+
+	// 수리완료
+	std::cout <<  "------------- 수리 완료 -------------" << std::endl;
+	default_clap.printStatus();
+	eunson_clap.printStatus();
+	hp_clap.printStatus();
+	energy_clap.printStatus();
+
+	// hp_clap Kill
+	std::cout <<  "------------- hp_clap 죽이기 -------------" << std::endl;
+	hp_clap.takeDamage(19);
+	hp_clap.beRepaired(1);
+	hp_clap.printStatus();
+
+	// energy_clap Kill
+	std::cout <<  "------------- hp_clap 죽이기 -------------" << std::endl;
+	for (int i = 0; i < 10; i++)
+		energy_clap.beRepaired(1);
+	energy_clap.printStatus();
+
+	// attack
+	default_clap.attack("minirt");
+	eunson_clap.attack("minirt");
+	hp_clap.attack("minirt");
+	energy_clap.attack("minirt");
+
 	return (0);
 }
